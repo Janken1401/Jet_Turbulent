29febc4984bb51788e307c258c5fba143a1f2ff4
Janken1401 <mel.tiar@univ-tlse3.fr>
2024-10-02 00:23:06 +0200

    Add a module to load .mat files

---
+++ read_datafiles.py
@@ -0,0 +1,9 @@
+from os.path import dirname, join as pjoin
+from scipy.io import loadmat
+
+data_dir = pjoin('Data', 'MeanFlow')
+mat_fname = pjoin(data_dir, 'mean_1.mat')
+
+mat_contents = loadmat(mat_fname)
+truc = mat_contents['arr']
+print(truc.dtype)
