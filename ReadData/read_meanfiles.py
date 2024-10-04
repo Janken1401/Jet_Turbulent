38d3f59f5b116bebc3dd868b81444d88e1f10637
Janken1401 <mel.tiar@univ-tlse3.fr>
2024-10-04 23:16:27 +0200

    Understood Data Files, created txt for it, stored modules for read data in a directory

--- ReadData/read_meanfiles.py
+++ ReadData/read_meanfiles.py
@@ -1,9 +1,15 @@
 from os.path import dirname, join as pjoin
 from scipy.io import loadmat
+import numpy as np
 
-data_dir = pjoin('Data', 'MeanFlow')
+data_dir = pjoin('../Data', 'MeanFlow')
 mat_fname = pjoin(data_dir, 'mean_1.mat')
 
 mat_contents = loadmat(mat_fname)
+
+header = mat_contents['__version__']
+
 truc = mat_contents['arr']
-print(truc)
+print(truc.shape)
+
+print(truc[535][68])
