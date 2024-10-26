18d5f6e7143c6ce6a31a3d57dabb9d851757dac5
Janken1401 <mel.tiar@univ-tlse3.fr>
2024-10-26 21:00:46 +0200

    Renamed constantes.py and added reference_values to setup nondimension field values

---
+++ src/toolbox/reference_values.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+r = 287.1 # Gas constant
+gamma = 1.4  # Heat Capacity ratio
+D = 0.038 # m
+p_0 = 98_000 # Pa
+T_0 = 288.15 # K
+
+
+rho_0 = p_0 / (r * T_0)
+c_0 = np.sqrt(gamma * r * T_0)
+
+T_s = (gamma - 1) * T_0 # Stagnation temperature
