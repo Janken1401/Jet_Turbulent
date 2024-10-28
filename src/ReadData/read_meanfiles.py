ca9094af2f80ff863e4939c7e097776214557f9d
Janken1401 <mel.tiar@univ-tlse3.fr>
2024-10-28 16:45:37 +0100

    Changed structure for class. Will be easiest to handle values

--- src/ReadData/read_meanfiles.py
+++ src/ReadData/read_meanfiles.py
@@ -3,78 +3,114 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
 from src.toolbox.path_directories import DIR_MEAN, RANS_FILES
+from toolbox.fig_parameters import RANS_FIGSIZE
+from src.ReadData.read_info import get_mach_reference
+
+rans_value_names = ['x', 'r', 'rho', 'ux', 'ur', 'ut', 'T', 'p']
 
 
-def get_RANS_values(ID_MACH):
-    """
-    Retrieve values of the RANS mean field based on the mach case selected
-    Parameters
-    ----------
-    ID_MACH
-        int: the mach case selected. Here ID_MACH = {1, 10}
-    Returns
-    -------
-    Dict: contains DataFrame for all the values e.g:
-        P = get_RANS_values(ID_MACH)['P'] retrieve the pressure field
-    """
-    names = ['x', 'r', 'rho', 'ux', 'ur', 'u_theta', 'T', 'P']
-    mean_file = loadmat(DIR_MEAN / RANS_FILES[ID_MACH])
-    data = mean_file['arr']
-    Nx, Nr, Nvalues = data.shape
+class ransField:
+    def __init__(self, ID_MACH):
+        """
 
-    return {name: pd.DataFrame(data[:, :, i], index=range(Nx), columns=range(Nr)) for i, name in enumerate(names)}
+        Parameters
+        ----------
+        ID_MACH
+        """
+        self.ID_MACH = ID_MACH
+        rans_file = DIR_MEAN / RANS_FILES[self.ID_MACH]
 
+        self.mach = get_mach_reference().loc[self.ID_MACH - 1]
+        self.rans_field_array = loadmat(rans_file)['arr']
+        self.Nx, self.Nr, self.Nvalues = self.rans_field_array.shape
+        self.rans_field = self.get_rans_values()
+        self.x = self.rans_field['x']
+        self.r = self.rans_field['r']
+        self.rho = self.rans_field['rho']
+        self.ux = self.rans_field['ux']
+        self.ur = self.rans_field['ur']
+        self.ut = self.rans_field['ut']
+        self.T = self.rans_field['T']
+        self.p = self.rans_field['p']
 
-def select_value_in_field(value, ID_MACH=1, x_max=10, r_max=3):
-    """
-    Return the value in the wanted domain
+    def get_rans_values(self):
+        """
+        Retrieve values of the RANS mean field based on the mach case selected
 
-    Parameters
-    ----------
-    value: DataFrame
-        values to retrieve in the wanted domain.
-        Value available : ux, ur, u_theta, T, P, Ma
-    x_max: DataFrame
-        maximum value on the x direction
-    r_max: DataFrame
-        maximum value o the radial direction
+        Returns
+        -------
+        Dict: a dict containing DataFrame for each value computed in the RANS field
+        """
+        return {name: pd.DataFrame(self.rans_field_array[:, :, i], index=range(self.Nx), columns=range(self.Nr))
+                for i, name in enumerate(rans_value_names)}
 
-    Returns
-    -------
-    x_sub: DataFrame
-        the x values in the wanted domain
-    r_sub: DataFrame
-        the r values in the wanted domain
-    value_sub: DataFrame
-        the values in the wanted domain
-    """
-    x = get_RANS_values(ID_MACH)['x']
-    r = get_RANS_values(ID_MACH)['r']
-    value = get_RANS_values(ID_MACH)[value]
-    x_mask = (x.iloc[:, 0] >= 0) & (x.iloc[:, 0] <= x_max)
-    r_mask = (r.iloc[0, :] >= 0) & (r.iloc[0, :] <= r_max)
+    def get_value_in_field(self, value, x_max=10, r_max=3):
+        """
+        Return the value in the wanted domain
 
-    # Apply the masks to select the subarray
-    x_sub = x.loc[x_mask, r_mask].values
-    r_sub = r.loc[x_mask, r_mask].values
-    value_sub = value.loc[x_mask, r_mask].values
-    return x_sub, r_sub, value_sub
+        Parameters
+        ----------
+        value: DataFrame
+            values to retrieve in the wanted domain.
+            Value available : x, r, rho, ux, ur, ut, T, p, Ma
+        x_max: int - optional
+            maximum value on the x direction
+        r_max: int - optional
+            maximum value on the radial direction
 
-def plot_value_in_field(x, r, value):
-    from toolbox.fig_parameters import RANS_FIGSIZE
-    plt.style.use('ggplot')
-    plt.figure(figsize=RANS_FIGSIZE, layout='constrained')
-    plt.contourf(x, r, value, levels=100, cmap='coolwarm')
-    plt.colorbar()
-    plt.show()
+        Returns
+        -------
+        x_sub: DataFrame
+            the x values in the wanted domain
+        r_sub: DataFrame
+            the r values in the wanted domain
+        value_sub: DataFrame
+            the values in the wanted domain
+        """
+        x_mask = (self.x.iloc[:, 0] >= 0) & (self.x.iloc[:, 0] <= x_max)
+        r_mask = (self.r.iloc[0, :] >= 0) & (self.r.iloc[0, :] <= r_max)
 
-x, r, ux = select_value_in_field('ux', 1)
-plot_value_in_field(x, r, ux)
+        # Apply the masks to select the subarray
+        x_sub = self.x.loc[x_mask, r_mask].values
+        r_sub = self.r.loc[x_mask, r_mask].values
+        value_sub = value.loc[x_mask, r_mask].values
+        return x_sub, r_sub, value_sub
 
+    def plot_x(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.x, x_max, r_max)
 
+    def plot_r(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.r, x_max, r_max)
 
+    def plot_rho(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.rho, x_max, r_max)
 
+    def plot_ux(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.ux, x_max, r_max)
 
+    def plot_ur(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.ur, x_max, r_max)
 
+    def plot_ut(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.ut, x_max, r_max)
 
+    def plot_T(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.T, x_max, r_max)
 
+    def plot_p(self, x_max=10, r_max=3):
+        self.plot_value_in_field(self.p, x_max, r_max)
+
+    def plot_value_in_field(self, value, x_max=10, r_max=3):
+        x, r, value = self.get_value_in_field(value, x_max, r_max)
+        plt.style.use('ggplot')
+        plt.figure(figsize=RANS_FIGSIZE, layout='constrained')
+        plt.contourf(x, r, value, levels=100, cmap='coolwarm')
+        plt.xlabel('x/D')
+        plt.ylabel('r/D')
+        plt.title(r'\hat{{self.value}}'.format(value))
+        plt.colorbar()
+        plt.show()
+
+rans_1 = ransField(1)
+
+rans_1.plot_p()
